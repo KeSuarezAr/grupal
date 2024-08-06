3d10f866d25be34cd825cae68e0dc469f26aa547
KeSuarezAr <kesuarezar@uide.edu.ec>
2024-08-06 18:19:49 -0500

    req

--- grupal/grupal.py
+++ grupal/grupal.py
@@ -1,14 +1,11 @@
 from reflex import App
 
-# from grupal.pages.courses_page import courses_page
-# from grupal.pages.login_page import login_page
-from grupal.pages.student_page import student_page
-from grupal.pages.user_list_page import users_page
-# from grupal.pages.trimester_list_page import trimesters_page
-from grupal.pages.asignaciones_list_page import asignments_page
 from .pages.index import index_pages
 from .pages.login import login_page
 from .pages.dashboard_padres import dashboard_page
+from grupal.pages.student_page import student_page
+from grupal.pages.user_list_page import users_page
+from grupal.pages.asignaciones_list_page import asignments_page
 
 app = App()
 
@@ -17,12 +14,5 @@ app.add_page(users_page)
 app.add_page(login_page)
 app.add_page(users_page)
 app.add_page(student_page)
-app.add_page(courses_page)
-# app.add_page(grades_page)
 app.add_page(asignments_page)
 app.add_page(dashboard_page)
-
-# app.add_page(login_page)
-# app.add_page(register_page)
-# app.add_page(trimesters_page)
-# app.add_page(index_page)
--- rxconfig.py
+++ rxconfig.py
@@ -1,10 +1,15 @@
-from reflex import Config
+import os
 
-port: int = 3306
-user: str = "root"
-host: str = "127.0.0.1"
-password: str = "password"
-database: str = "grupal"
+from reflex import Config
+from dotenv import load_dotenv
+
+load_dotenv()
+
+port: int = os.getenv("DBPORT", 3306)
+user: str = os.getenv("DBUSER")
+host: str = os.getenv("DBHOST")
+password: str = os.getenv("DBPASS")
+database: str = os.getenv("DBNAME")
 
 config = Config(
     app_name="grupal",
